6240fd3cae9d50ab7f330ea886174a340a823851
NicoCB <nicolas.gabbani@careerbuilder.com>
2016-04-27 12:24:27 +0200

    #1

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+  $('.collapse').click(function(){
+    $(this).find('i.fa').toggleClass('collapse-active');
+    $(this).next('.collapse-content').slideToggle();
+  });
+
+  $('#rdb_tools_button').click(function(){
+    $('#rdb_tools').slideToggle();
+  })
+
+});
