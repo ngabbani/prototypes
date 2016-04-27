5dc65df5446d6113feb65162a9105f04be35e413
NicoCB <nicolas.gabbani@careerbuilder.com>
2016-04-27 15:07:42 +0200

    fix

--- rdb/js/app.js
+++ rdb/js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
-  $('.collapse').click(function(){
-    $(this).find('i.fa').toggleClass('collapse-active');
-    $(this).next('.collapse-content').slideToggle();
-  });
+  // $('.collapse').click(function(){
+  //   $(this).find('i.fa').toggleClass('collapse-active');
+  //   $(this).next('.collapse-content').slideToggle();
+  // });
 
   $('#rdb_tools_button').click(function(){
     $('#rdb_tools').slideToggle();
