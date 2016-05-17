b174e176f7d9f1192269a83cf33017cdb54a7d79
NicoCB <nicolas.gabbani@careerbuilder.com>
2016-05-17 10:47:45 +0200

    list old job

---
+++ filiste/filiste/hotjar-168803.js
@@ -0,0 +1,9 @@
+window.hjSiteSettings = {"testers_widgets":[],"surveys":[],"heatmaps":[{"created_epoch_time":1459344536,"targeting":[{"negate":false,"pattern":"http:\/\/www.lesjeudis.com\/magazine-informatique\/metiers-informatiques","match_operation":"starts_with","component":"url"}],"id":440419}],"recording_capture_keystrokes":true,"polls":[],"site_id":168803,"record_targeting_rules":[],"forms":[],"record":false,"r":0.0850598958,"deferred_page_contents":[{"targeting":[{"negate":false,"pattern":"jobposter\/resumes\/resumeresults.aspx","match_operation":"contains","component":"url"},{"negate":false,"pattern":"phone","match_operation":"exact","component":"device"}],"id":1116170},{"targeting":[{"negate":false,"pattern":"http:\/\/www.lesjeudis.com\/magazine-informatique\/metiers-informatiques","match_operation":"starts_with","component":"url"},{"negate":false,"pattern":"desktop","match_operation":"exact","component":"device"}],"id":945958},{"targeting":[{"negate":false,"pattern":"http:\/\/www.lesjeudis.com\/magazine-informatique\/metiers-informatiques","match_operation":"starts_with","component":"url"},{"negate":false,"pattern":"tablet","match_operation":"exact","component":"device"}],"id":945957},{"targeting":[{"negate":false,"pattern":"http:\/\/www.lesjeudis.com\/magazine-informatique\/metiers-informatiques","match_operation":"starts_with","component":"url"},{"negate":false,"pattern":"phone","match_operation":"exact","component":"device"}],"id":945956}]};
+
+window.hjBootstrap = function (scriptUrl) {
+    var s = document.createElement('script');
+    s.src = scriptUrl;
+    document.getElementsByTagName('head')[0].appendChild(s);
+};
+
+hjBootstrap('https://script.hotjar.com/modules-be97440279b42c47fc7a28200eb90b8c.js');
