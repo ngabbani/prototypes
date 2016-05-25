bb864ba1642d261cbdefaf9b678ac6b0b949cece
NicoCB <nicolas.gabbani@careerbuilder.com>
2016-05-25 11:51:02 +0200

    filter

--- newrdb/fime/new_app.js
+++ newrdb/fime/new_app.js
@@ -2,4 +2,19 @@ $(document).ready(function(){
   $('.tt').tooltipster({
     theme: 'tooltipster-light'
   });
+
+  $('#filter-button').click(function(e){
+    e.preventDefault();
+    $('#jobs-list-push, #jobs-list-content').toggleClass('active')
+  });
+
+
+  $button = $('.filter').find('filter-title');
+  $icon = $button.find('.filter-button');
+  $content = $('.filter').find('.filter-content');
+
+  $('.filter .filter-title').click(function(){
+    $(this).find('.filter-button').toggleClass('fa-plus fa-minus');
+    $(this).parent().find('.filter-content').slideToggle();
+  });
 });
