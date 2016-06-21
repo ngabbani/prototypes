8e5468fb98beb4fd1b07625a5fc57f1b4c6288e3
NicoCB <nicolas.gabbani@careerbuilder.com>
2016-06-21 11:51:35 +0200

    rdb form

---
+++ rdbform/form/new_app.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  $('#filter-button').click(function(e){
+    e.preventDefault();
+    height = $('#jobs-list-filter').height() + 60
+    $('#jobs-list-push').toggleClass('active')
+    if(!$('#jobs-list-push').hasClass('active')){
+      $('#jobs-list-filter').css('margin-top', height * -1 + 'px');
+    } else {
+      $('#jobs-list-filter').css('margin-top', 0 + 'px');
+    }
+  });
+
+  $('.filter .filter-title').click(function(){
+    $(this).find('.filter-button').toggleClass('fa-plus fa-minus');
+    $(this).parent().find('.filter-content').slideToggle();
+  });
+});
