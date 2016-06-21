23228376367c71d888fa77f192c6cf885795f964
NicoCB <nicolas.gabbani@careerbuilder.com>
2016-06-21 15:27:53 +0200

    one shot folder

--- rdb/assets/new_app.js
+++ rdb/assets/new_app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-
+  $('.tt').tooltipster();
+  
   $('#jobs-list-filter').css('margin-top', ($('#jobs-list-filter').height() + 60) * -1 + 'px');
 
   $('#filter-button').click(function(e){
@@ -16,5 +17,23 @@ $(document).ready(function(){
   $('.filter .filter-title').click(function(){
     $(this).find('.filter-button').toggleClass('fa-plus fa-minus');
     $(this).parent().find('.filter-content').slideToggle();
+  });
+
+  $('#rdb_tools_button').click(function(){
+    $('#rdb_tools').slideToggle();
+  });
+
+  var $navbar = $(".sticky-nav, #resume_details"),
+        y_pos = $navbar.offset().top,
+        height = $navbar.height();
+
+    $(document).scroll(function() {
+      var scrollTop = $(this).scrollTop();
+
+      if (scrollTop > y_pos) {
+          $navbar.addClass("fixed");
+      } else if (scrollTop <= y_pos) {
+          $navbar.removeClass("fixed");
+      }
   });
 });
